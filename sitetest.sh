5eaeb7386b2519ad8059cd41fdeff455a02433c0
mrbbowden <bbowden@gmail.com>
2021-11-09 09:56:27 -0500

    add sitetest.sh

--- sitetest.sh
+++ sitetest.sh
@@ -1,9 +1,29 @@
 :
+if [ -z ${1+x} ];
+then
+ echo "usage: sitetest.sh ipaddress"
+ exit
+fi
 ip=$1
+correctcount=0
 actions=(read download scooby)
 for theaction in ${actions[@]}; do
-echo "|$theaction|"
-curl -s -o /dev/null -w "$ip $theaction %{http_code}\n" http://$ip/manage_file \
-   -H 'Content-Type: application/json' \
-   -d "{\"action\":  \"$theaction\"}"
+#curl -s -o /dev/null -w "$ip $theaction %{http_code}\n" http://$ip/manage_file \
+curl_status=$(curl -s -o /dev/null -H  'Content-Type: application/json' -d "{\"action\":  \"$theaction\"}" -w '%{http_code}' --connect-timeout 3 http://$ip/manage_file)
+echo "$ip $theaction $curl_status"
+if [ $theaction  ==  "scooby" -a $curl_status -eq 500 ]
+then
+ ((correctcount=correctcount+1))
+elif [ $theaction != "scooby" -a $curl_status -eq 200 ]
+then
+ ((correctcount=correctcount+1))
+fi
+done
+if [ $correctcount -eq 3 ]
+then
+ echo "correctcount is $correctcount tests pass"
+else
+ echo "correctcount is $correctcount tests fail"
+fi
+ $theaction $curl_status"
 done
